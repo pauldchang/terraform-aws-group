a2e143f3f35831240485cacd5b68fb5fe3073f75
pauldchang <153890789+pauldchang@users.noreply.github.com>
2024-05-07 21:00:10 -0500

    data

---
+++ ami-data.tf
@@ -0,0 +1,12 @@
+data "aws_ami" "my_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+    tags = {
+    Name = "Data_instance"
+  }
+}
