12fbdca6527994b736c70cd8dbe1005822e3cd8f
pauldchang <153890789+pauldchang@users.noreply.github.com>
2024-05-07 20:46:20 -0500

    autoscaling group

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,38 @@
 resource "aws_instance" "my_launch_template" {
   launch_template {
+    count   = 3
     id      = aws_launch_template.my_launch_template.id
     version = "$Latest"  
   }
     tags = {
-    Name = "launch_templaate_instance"
+    Name = "launch_template_instance"
   }
 }
 resource "aws_instance" "Data_instance" {
+  count         = 2
   ami           = data.aws_ami.my_ami.id
   instance_type = "t2.micro"
   key_name      = "SSH-MAC"  
   subnet_id     = "subnet-00468707d5bab7a1a"  
   tags = {
     Name = "data_instance"
+  }
+}
+
+resource "aws_launch_template" "my_launch_template" {
+  name_prefix   = "my_launch_template"
+  image_id      = "ami-0900fe555666598a2"
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "bar" {
+  availability_zones = ["us-east-2a"]
+  desired_capacity   = 3
+  max_size           = 5
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.group.id
+    version = "$Latest"
   }
 }
