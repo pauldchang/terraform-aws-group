e6140c8ee7108c51bac3011025974f77955e60cc
pauldchang <153890789+pauldchang@users.noreply.github.com>
2024-05-08 17:59:19 -0500

    UPDATE

--- ami-data.tf
+++ ami-data.tf
@@ -1,22 +1,9 @@
-data "aws_ami" "my_ami" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-    tags = {
-    Name = "Data_instance"
-  }
-}
-
 data "aws_ami" "my_launch_template1" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["my-ami-pattern-1"]
+    values = ["ami-0900fe555666598a2"] 
   }
 
   filter {
@@ -30,7 +17,7 @@ data "aws_ami" "my_launch_template2" {
 
   filter {
     name   = "name"
-    values = ["my-ami-pattern-2"]
+    values = ["ami-0900fe555666598a2"]  
   }
 
   filter {
