c8219492ac8b59ecb970a3d27532b3dba59d9e68
pauldchang <153890789+pauldchang@users.noreply.github.com>
2024-05-08 17:54:51 -0500

    change

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,15 @@
-resource "aws_launch_template" "my_ec2_launch_template" {
+resource "aws_launch_template" "my_ec2_launch_template1" {
   name_prefix   = "my_ec2_launch_template"
   image_id      = data.aws_ami.my_launch_template1.id  
   instance_type = "t2.micro"
 }
-resource "aws_launch_template" "my_ec2_launch_template" {
+
+resource "aws_launch_template" "my_ec2_launch_template2" {
   name_prefix   = "my_ec2_launch_template"
   image_id      = data.aws_ami.my_launch_template2.id  
   instance_type = "t2.micro"
 }
+
 
 # resource "aws_launch_template" "my_launch_template1" {
 #   name_prefix   = "my_launch_template1"
