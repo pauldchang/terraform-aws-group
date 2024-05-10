6a00ae6b61acc0fd9ebb6e8ec8d555045ad8fc46
pauldchang <153890789+pauldchang@users.noreply.github.com>
2024-05-09 20:23:48 -0500

    update to main and backend s3

--- module/main.tf
+++ module/main.tf
@@ -1,28 +1,64 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
+resource "aws_subnet" "public" {
+  count                  = 3
+  vpc_id                 = aws_vpc.main.id
+  cidr_block             = "10.0.${count.index}.0/24"
+ 
+}
 
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+resource "aws_subnet" "private" {
+  count                  = 3
+  vpc_id                 = aws_vpc.main.id
+  cidr_block             = "10.0.${count.index + 3}.0/24"
+}
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
+resource "aws_nat_gateway" "nat" {
+  count             = 3
+  subnet_id         = aws_subnet.private[count.index].id
+  allocation_id     = aws_eip.nat[count.index].id
+}
+
+resource "aws_eip" "nat" {
+  count = 3
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
-}
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
 
-  # The rest of arguments are omitted for brevity
-
-  enable_nat_gateway  = true
-  single_nat_gateway  = false
-  reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
-  external_nat_ip_ids = "${aws_eip.nat.*.id}"   # <= IPs specified here as input to the module
 }
+
+
+
+resource "aws_route_table" "private" {
+   count = 3
+  vpc_id = aws_vpc.main.id
+ 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat[count.index].id
+  }
+
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 3
+  subnet_id      = "${element(aws_subnet.public..id, count.index)}"
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = 3
+  subnet_id      = "${element(aws_subnet.private..id, count.index)}" 
+  route_table_id = aws_route_table.private[count.index].id
--- var.tf
+++ var.tf
@@ -1,19 +1,19 @@
-variable "cidr_block" {
-  type    = string
-  default = "10.0.0.0/16"
-}
+# variable "cidr_block" {
+#   type    = string
+#   default = "10.0.0.0/16"
+# }
 
-variable "num_public_subnets" {
-  type    = number
-  default = 3
-}
+# variable "num_public_subnets" {
+#   type    = number
+#   default = 3
+# }
 
-variable "num_private_subnets" {
-  type    = number
-  default = 3
-}
+# variable "num_private_subnets" {
+#   type    = number
+#   default = 3
+# }
 
-variable "internet_gateway_name" {
-  type    = string
-  default = "my-internet-gateway"
-}
+# variable "internet_gateway_name" {
+#   type    = string
+#   default = "my-internet-gateway"
+# }
