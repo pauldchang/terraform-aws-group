173c9696bf161524331e49a5917629a12dfa9eef
pauldchang <153890789+pauldchang@users.noreply.github.com>
2024-05-13 20:48:47 -0500

    var

---
+++ module/vpc/var.tf
@@ -0,0 +1,17 @@
+variable "db_name" {
+  description = "Name of the Database."
+  type        = string
+  default = "wordpressdb"
+}
+
+variable "db_username" {
+  description = "Master user name"
+  type        = string
+  default = "wordpress"
+}
+
+variable "db_password" {
+  description = "Master password"
+  type        = string
+  default = "password"
+}
